e7b8f9acca8f5e50e4f36a2883976d771ba54b79
dhruv <dhruv7811@gmail.com>
2024-08-26 13:34:22 +0530

    Complete Project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from youtube_transcript_api import YouTubeTranscriptApi
+
+app = Flask(__name__)
+
+@app.get('/summary')
+def summary_api():
+    url = request.args.get('url', '')
+    video_id = url.split('=')[1]
+    summary = get_transcript(video_id)
+    return summary, 200
+
+def get_transcript(video_id):
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_list])
+    return transcript
+    
+
+if __name__ == '__main__':
+    app.run()
